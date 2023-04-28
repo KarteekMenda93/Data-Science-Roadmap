10a5bfde477446def1e81b7c721442202a715f06
Karteek Menda <karteek.menda19@st.niituniversity.in>
2023-04-28 12:59:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.title('🎈 Presentation App')
+st.title('🎈 Data Science RoadMap')
 components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vQYEXPK4JBVPMohjaKAzGgQE7JNuc_MkMT3AsHekiO-nL7rtxmAn98a7YD1AdNMiTP6jPdpCBCLGWg-/embed?start=false&loop=false&delayms=3000", height=432)
